87f662ec861e581397926203e388ca057f28ba54
Jeremie Lim <jlim@morphlabs.com>
2012-12-18 16:22:17 +0800

    added js for fixed/float menu

--- v4/js/main.js
+++ v4/js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 	var wHeight = $(window).outerHeight() - $('#footer-wrap').outerHeight() - $('#head-wrap').outerHeight() - $('#mainnav-wrap').outerHeight();
 	
+	var top = $('#tab-wrap').offset().top - parseFloat($('#tab-wrap').css('marginTop').replace(/auto/, 0));
+	
+	$('#details').scroll(function (event) {
+		
+		// what the y position of the scroll is
+		var y = $(this).scrollTop();
+		
+		if (y >= top) {
+		  
+		  $('#tab-wrap').addClass('fixed');
+		} else {
+		  
+		  $('#tab-wrap').removeClass('fixed');
+		}
+	});
+	
 	$('#instances').height(wHeight);
 	$('#details').height(wHeight);
 
